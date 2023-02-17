5f6721f1279aa588bb8c1d0d7c946ce456b017c1
nfcdjobo <nfcdjobo@gmail.com>
2023-02-17 14:26:01 +0000

    Programme pour déterminer la factorielle d'un nombre entier

---
+++ factorielle.js
@@ -0,0 +1,28 @@
+function calcul_Factorielle(para) {
+    let calculette = 1;
+    if (!isNaN(para) && Number.isInteger(para)) {
+        if (para < 0) {
+            calculette = -Math.abs(para) * calcul_Factorielle(Math.abs(para) - 1);
+        } else if (para == 0) {
+            calculette = 1;
+        } else {
+            calculette = para * calcul_Factorielle(para - 1);
+        }
+        document.getElementById("resul").textContent = para + "! = ";
+        document.getElementById("resultat").value = calculette;
+        document.getElementById("erreur").textContent = "";
+        document.getElementById("message").textContent = "";
+        return calculette;
+    } else {
+        document.getElementById("resul").textContent = "";
+        document.getElementById("resultat").value = "Error 2";
+        document.getElementById("erreur").textContent = "Erreur : ";
+        document.getElementById("message").textContent = "La valeur saisie n'est pas valide pour le calcul de factorielle";
+        return "La valeur saisie n'est pas valide pour le calcul de factorielle";
+    }
+}
+function ecouteur() {
+    let argu = parseInt(document.getElementById("nombre").value);
+    calcul_Factorielle(argu);
+}
+document.getElementById("nombre").addEventListener("input", ecouteur);
